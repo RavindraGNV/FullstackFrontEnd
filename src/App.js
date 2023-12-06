3778f2bb2ee65e324d4cdf888822b0e3c7a57d9f
ravindra <you@example.com>
2023-12-06 18:16:15 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Appbar from './Appbar';
+import Student from './student';
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar />
+      <Student />
+    </div>
+  );
+}
+
+export default App;
